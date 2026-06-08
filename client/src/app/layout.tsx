7f41fe32f72900c8a1f92907e9bf85abe9bf9e90
MigrainePanda <nicktanaka36@gmail.com>
2026-06-07 17:15:53 -0700

    fix err caused from unused loaded fonts from layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,20 +1,10 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono, Inter } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { ThemeProvider } from '@/components/theme/theme-provider';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Manga Tracker',
@@ -29,12 +19,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn(
-        'font-sans',
-        inter.variable,
-        geistSans.variable,
-        geistMono.variable,
-      )}
+      className={cn('font-sans', inter.variable)}
       suppressHydrationWarning
     >
       <body className={`antialiased min-h-screen`}>
